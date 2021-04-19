470dfeed469bc98e1c8005294e41696bf748c68d
ahmadmoaaz2 <46950624+ahmadmoaaz2@users.noreply.github.com>
2021-04-19 10:49:56 -0700

    export redux functions

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+        </div>
+    );
 }
 
 export default Home
--- frontend/src/utils/redux-store.js
+++ frontend/src/utils/redux-store.js
@@ -12,7 +12,7 @@ const initialState = {
 function reducer(state = initialState, action){
     switch (action.type){
         case LOGIN:
-            const {jwt, user} = action.payload;
+            const {jwt, user} = action.data;
             return {...state, jwt, user};
         case LOGOUT:
             return {...state, jwt: "", user: ""};
@@ -20,5 +20,6 @@ function reducer(state = initialState, action){
 }
 
 const store = createStore(reducer);
+export const {dispatch, getState, subscribe} = store;
 
 export default store;
