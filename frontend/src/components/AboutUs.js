08a91e30cef693a0ed950ec339d02ab42ca457d9
Kevin Yoo <k.ysyoobcit@gmail.com>
2021-04-23 12:35:16 -0700

    Navbar_modified

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -3,4 +3,5 @@ import {makeStyles} from "@material-ui/core/styles";
 import {Launch} from "@material-ui/icons";
 import {Container, Grid, Card, CardActionArea, CardMedia, CardContent, Typography, Button} from "@material-ui/core";
 
+
 export default AboutUs;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,77 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { makeStyles } from "@material-ui/core/styles";
 import '../styling/style.css'
+import {Container, Grid, Card, CardActionArea, CardMedia, CardContent, Typography, Button} from "@material-ui/core";
 
 function Navbar() {
+    const useStyles = makeStyles({
+        NavbarList: {
+            alignContent: 'center'
+            
+        },
+        a:{
+
+        },
+        navLink: {
+            textDecoration: 'none',
+            color: 'black',
+            fontSize:'30px',
+            paddingBottom:'5px',
+            margin:'18px',
+            borderBottom: '1px solid #B78C48',
+            '&:hover': {
+                color: "#B78C48",
+                borderBottom:"#B78C48"
+            }
+        },
+        menuText: {
+            
+            display:"inline",
+            textAlign:"center",
+            marginBlockStart: "4px",
+            marginBlockEnd: "4px"
+        },
+        navBlock: {
+            textAlign:'center'
+        }   
+            
+        });
+    const classes = useStyles();
     return (
-        <div>
-            <ul className="NavbarList">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
+        <Container maxWidth={"lg"}>
+        <div className={classes.navBlock}>
+                <p className={classes.menuText}>
+                    <a className={classes.navLink}>
+                        Home
+                    </a>
+                </p>
+                <p className={classes.menuText}>
+                    <a className={classes.navLink}>
+                        About
+                    </a>
+                </p>
+                <p className={classes.menuText}>
+                    <a className={classes.navLink}>
+                        Projects
+                    </a>
+                </p>
+                <p className={classes.menuText}>
+                    <a href="/login" className={classes.navLink}>
+                        login
+                    </a>
+                </p>
+                <p className={classes.menuText}>
+                    <a href="/register" className={classes.navLink}>
+                        Register
+                    </a>
+                </p>
+                <p className={classes.menuText}>
+                    <a className={classes.navLink}>
+                        Contact
+                    </a>
+                </p>
+                {/* <li>
                     <Link to="/">Projects</Link>
                 </li>
                 <li>
@@ -24,9 +86,13 @@ function Navbar() {
                 </li>
                 <li>
                     <Link to="/">Contact</Link>
-                </li>
-            </ul>
+                </li> */}
         </div>
+
+
+        </Container>
+
+        
     )
 }
 
